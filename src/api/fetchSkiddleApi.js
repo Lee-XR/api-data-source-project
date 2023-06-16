9fd622a62d29d915d790a4632cfdedb4b7856e24
Lee-XR <xian.rong1124@gmail.com>
2023-06-16 19:34:47 +0800

    create axios instance in file

--- src/api/fetchSkiddleApi.js
+++ src/api/fetchSkiddleApi.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
+const instance = axios.create({
+	baseURL:
+		import.meta.env.MODE === 'production'
+			? import.meta.env.VITE_BACKEND_BASE_URL_PROD
+			: import.meta.env.VITE_BACKEND_BASE_URL_DEV,
+});
+
 // Single fetch promise for Skiddle API
 export async function fetchSkiddle(type, id, params) {
-
-	return await axios
-		.post('api/skiddle', {type, id, params})
+	return await instance
+		.post('api/skiddle', { type, id, params })
 		.then((response) => {
 			return response.data;
 		})
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './styles/index.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	// <React.StrictMode>
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 	// </React.StrictMode>,
 );
 
-axios.defaults.baseURL =
-	import.meta.env.MODE === 'production'
-		? import.meta.env.VITE_BACKEND_BASE_URL_PROD
-		: import.meta.env.VITE_BACKEND_BASE_URL_DEV;
+// axios.defaults.baseURL =
+// 	import.meta.env.MODE === 'production'
+// 		? import.meta.env.VITE_BACKEND_BASE_URL_PROD
+// 		: import.meta.env.VITE_BACKEND_BASE_URL_DEV;
